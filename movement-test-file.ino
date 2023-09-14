10c22ffa3d003f5f71aeea3174cc2afc5550f2bc
josssss07 <joshuadesai03@gmail.com>
2023-09-15 00:35:50 +0530

    Jesus christ waht tf

--- movement-test-file.ino
+++ movement-test-file.ino
@@ -28,3 +28,35 @@ void loop() {
       digitalWrite(pwm2, 0);
     }
 }
+
+
+
+
+//example 2
+
+
+// This program translates a value that is read from a potentiometer into a PWM value
+// to drive a motor using the Pololu Simple Motor Controller.
+#include <Servo.h> 
+
+Servo motor;
+
+// constants won't change.
+#define PIN_POTENTIOMETER 2
+#define PIN_PWM 9
+
+void setup()
+{motor.attach(PIN_PWM);
+}
+
+void loop()
+{int potValue = analogRead(PIN_POTENTIOMETER);  // leitura do potenciômetro
+ int speed = map(potValue, 0, 1023, 0, 180);
+  
+ if( speed==90) motor.write(LOW);
+   
+ delay(30); // a cada 30 ms
+ motor.write(speed); // atualiza a velocidade do motor
+  
+ delay(300); // pausa de 300 ms
+}
