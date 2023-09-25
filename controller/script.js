492604c6c1424d5c7444b787c7d49c2fe900ebb8
Castel <castelshal@gmail.com>
2023-09-25 18:43:29 +0530

    Updating the controller files

--- controller/script.js
+++ controller/script.js
@@ -1,18 +1,21 @@
 let gateway = `ws://${window.location.hostname}/ws`;
 let websocket;
+let cmdMap = {};
+const inputs = [...document.querySelectorAll("input.btn")];
 
 function onLoad(){
     initWebSocket();
 }
-
 window.addEventListener('load', onLoad);
 
-function sendForward(){
-    websocket.send("f");
+function sendCmd(arg){
+    websocket.send(arg);
+    document.querySelector("h1").textContent = arg;
 }
 
 function stop(){
     websocket.send("s");
+    document.querySelector("h1").textContent = "s";
 }
 
 function initWebSocket(){
@@ -25,9 +28,24 @@ function initWebSocket(){
 
 function onOpen(){
     console.log("Connection etablished: ");
-    document.querySelector("#ip").onmousedown = sendForward;
-    document.querySelector("#ip").onmouseup = stop;
+    inputs.forEach(elem => {
+        elem.onmousedown = sendCmd.bind(this, elem.id);
+        elem.ontouchstart = sendCmd.bind(this, elem.id);
+        if (elem.id == "l" || elem.id == "r"){    
+            elem.onmouseup = sendCmd.bind(this, "ts");
+            elem.onmouseout = sendCmd.bind(this, "ts");
+            elem.ontouchmove = sendCmd.bind(this, "ts");
+            elem.ontouchend = sendCmd.bind(this, "ts");
+        }
+        else{
+            elem.onmouseup = stop;
+            elem.onmouseout = stop;
+            elem.ontouchmove = stop;
+            elem.ontouchend = stop;
+        }
+    })
 }
+
 
 function onClose(){
     console.log("Connection lost: ");
@@ -38,3 +56,4 @@ function onMessage(event){
     console.log("Message received: ");
     console.log
 }
+
